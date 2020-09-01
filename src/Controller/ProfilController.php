a10a8873b20d1c8caf506da8b4f201e4952316ac
percybazile <percybazile@gmail.com>
2020-09-01 17:02:48 +0200

    exo 1

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,31 +2,34 @@
 
 namespace App\Controller;
 
+use App\Form\UserPasswordFormType;
+use App\Form\UserProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfilController extends AbstractController
 {
     /**
      * @Route("/profil", name="profil")
      */
-    public function index()
+    public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProfilController.php',
+        // Formulaire de modifications des infos du profil
+        $profileForm = $this->createForm(UserProfileFormType::class, $this->getUser());
+        $profileForm->handleRequest($request);
+
+        if ($profileForm->isSubmitted() && $profileForm->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Vos informations ont été mises à jour.');
+        }
+
+        return $this->render('profile/profil.html.twig', [
+            'profile_form' => $profileForm->createView(),
         ]);
     }
-     /**
-     * @Route("/event", name="event_made")
-     */
-
-   
-
-
-
-      /**
-     * @Route("/events", name="event_participate")
-     */
     
 }
